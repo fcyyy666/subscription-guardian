5c0013903328d1e7282c817c5b529b5690960d3c
16488 <1648830428@qq.com>
2026-02-11 21:31:01 +0800

    feat: Initialize Drizzle ORM database connection using postgres-js and schema.

--- db/index.ts
+++ db/index.ts
@@ -2,7 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL!;
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+}
+const connectionString = process.env.DATABASE_URL;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
